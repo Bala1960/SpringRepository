80fcbb805e8fec94a2059362fe7ed63a1abfe909
Bala1960 <athreyaOne@gmail.com>
2023-09-18 05:10:08 +0530

    FirstSpringCommit

---
+++ src/test/java/com/DatabaseValidation/ValidationDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DatabaseValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
